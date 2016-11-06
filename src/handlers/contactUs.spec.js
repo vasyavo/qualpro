11e7fb4ccd0e0b8f02d0e6970417302679986e07
Yuriy Derevenko <jedprankster7@gmail.com>
2016-11-06 17:15:32 +0200

    test(src/handlers): very basic test for contactus form

---
+++ src/handlers/contactUs.spec.js
@@ -0,0 +1,70 @@
+'use strict';
+
+const expect = require('chai').expect;
+const request = require('supertest-as-promised');
+const server = require('./../server');
+const faker = require('faker');
+const agent = request.agent(server);
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const PersonnelModel = require('../types/personnel/model');
+
+
+describe('feature: contactUs form', () => {
+    const password = faker.lorem.words(1);
+    const login = faker.internet.email().toLowerCase();
+    const salt = bcrypt.genSaltSync(10);
+    const su = {
+        login : login,
+        email : login,
+        pass : bcrypt.hashSync(password, salt),
+        super : true
+    };
+    const contactUsForm = {
+        type : faker.random.arrayElement([
+            'Application Related Issue',
+            'Future Application Ideas',
+            'Others'
+        ]),
+        description : faker.lorem.paragraph()
+    };
+
+    before(function *() {
+        yield Promise.all([
+            PersonnelModel.collection.insert(su)
+        ])
+    });
+
+    beforeEach(function *() {
+        yield Promise.all([
+            agent
+                .post('/login')
+                .send({
+                    login : login,
+                    pass : password
+                }),
+            agent
+                .post('mobile/login')
+                .send({
+                    login : login,
+                    pass : password
+                })
+        ]);
+    });
+
+    it('should get all existing contactUs form', function *() {
+        const resp = yield agent
+            .get('/contactUs')
+            .send()
+            .expect(200);
+
+        const body = resp.body;
+
+        expect(body).to.be.an('Object');
+        expect(body).to.have.property('total', 0);
+        expect(body).to.have.property('data');
+        expect(body.data).to.be.an('Array');
+    });
+
+});
