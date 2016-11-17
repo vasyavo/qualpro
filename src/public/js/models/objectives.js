9aa51341b00d6a4c5e6cc1e64b15da8739107dd0
Ivan Ferenchuk <ivan.ferenchuk@gmail.com>
2016-11-17 16:56:22 +0200

    fix(Objectives): locations have many arrows

--- src/public/js/models/objectives.js
+++ src/public/js/models/objectives.js
@@ -94,16 +94,21 @@ define([
             return CONTENT_TYPES.OBJECTIVES;
         },
         modelParse: function (model) {
+            var location = [];
             var currentLanguage = App && App.currentUser && App.currentUser.currentLanguage ? App.currentUser.currentLanguage : 'en';
-
             var countryString = this.modelMapper('country', model, currentLanguage);
             var regionString = this.modelMapper('region', model, currentLanguage);
             var subRegionString = this.modelMapper('subRegion', model, currentLanguage);
             var retailSegmentString = this.modelMapper('retailSegment', model, currentLanguage);
             var outletString = this.modelMapper('outlet', model, currentLanguage);
             var branchString = this.modelMapper('branch', model, currentLanguage);
-
-            model.location = countryString + ' > ' + regionString + ' > ' + subRegionString + ' > ' + retailSegmentString + ' > ' + outletString + ' > ' + branchString;
+            countryString ? location.push(countryString) : '';
+            regionString ? location.push(regionString) : '';
+            subRegionString ? location.push(subRegionString) : '';
+            retailSegmentString ? location.push(retailSegmentString) : '';
+            outletString ? location.push(outletString) : '';
+            branchString ? location.push(branchString) : '';
+            model.location = location.join(' > ');
 
             _.map(CONSTANTS.OBJECTIVESTATUSES_FOR_UI, function (status) {
                 if (status._id === model.status) {
