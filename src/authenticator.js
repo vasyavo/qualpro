078eda4d4c22e594209979720705008646fbab9a
oleksijfomin <oleksij.fomin@gmail.com>
2016-12-06 01:23:43 +0200

    fix(questionnary): retrieve pages and entire list relates to session user.

--- src/authenticator.js
+++ src/authenticator.js
@@ -5,10 +5,12 @@ const agentSu = request.agent(server);
 const agentMaster = request.agent(server);
 const countryAdmin = request.agent(server);
 const areaManager = request.agent(server);
+const merchandiser = request.agent(server);
 
 module.exports = {
     su: agentSu,
     master: agentMaster,
     countryAdmin,
-    areaManager
+    areaManager,
+    merchandiser
 };
