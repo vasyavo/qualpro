147c46fab0e741c1a36f2b50aa01c31563948d5f
ferenchuk <ivan.ferenchuk@gmail.com>
2016-12-27 13:00:02 +0200

    fix(InStoreTasks): Fixed Assign button visibility when task is failed
    (cherry picked from commit 9093324)

--- src/public/js/views/inStoreTasks/preView/preView.js
+++ src/public/js/views/inStoreTasks/preView/preView.js
@@ -690,7 +690,7 @@ define([
                 });
             }
 
-            if ([CONSTANTS.OBJECTIVE_STATUSES.CLOSED, CONSTANTS.OBJECTIVE_STATUSES.COMPLETED].indexOf(jsonModel.status._id) === -1 && App.currentUser.workAccess) {
+            if ([CONSTANTS.OBJECTIVE_STATUSES.CLOSED, CONSTANTS.OBJECTIVE_STATUSES.COMPLETED, CONSTANTS.OBJECTIVE_STATUSES.OVER_DUE, CONSTANTS.OBJECTIVE_STATUSES.FAIL].indexOf(jsonModel.status._id) === -1 && App.currentUser.workAccess) {
                 if (!this.activityList) {
                     configForTemplate.forEach(function (config) {
                         var createdByMe = jsonModel.createdBy.user._id === App.currentUser._id;
