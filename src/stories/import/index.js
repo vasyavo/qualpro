eb2f72cf8fea933b3cdf3a0866237234c100d263
oleksijfomin <oleksij.fomin@gmail.com>
2017-01-10 21:43:22 +0200

    feat(import): add new import data for "Item"

--- src/stories/import/index.js
+++ src/stories/import/index.js
@@ -4,14 +4,15 @@ mongoose.Schemas = {};
 require('./../../utils/mongo');
 
 const importOrigin = require('./types/origin');
+const importItem = require('./types/item');
 const logger = require('./../../utils/logger');
 
-importOrigin((err) => {
+importItem((err, data) => {
     if (err) {
-        logger.error('Import of "Origin" failed.');
+        logger.error('Import failed.');
         process.exit(1);
     }
 
-    logger.info('Import of "Origin" done.');
+    logger.info('Import done.');
     process.exit(0);
 });
