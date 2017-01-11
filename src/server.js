149f1a01b0d2d307f26cf55cfd684def0c5464bd
ivanlyah <johnnye.be@gmail.com>
2017-01-11 18:03:28 +0200

    hotfix(server): revert to cluster mode

--- src/server.js
+++ src/server.js
@@ -1,29 +1,26 @@
 const throng = require('throng');
 const config = require('./config');
 
-//function startWorker(id) {
-//    console.log(`Started worker ${ id }`);
-//
-//    process.on('SIGTERM', () => {
-//        console.log(`Worker ${ id } exiting...`);
-//        console.log('(cleanup would happen here)');
-//        process.exit();
-//    });
-//
-//    require('./app');
-//}
-//
-//function startMaster() {
-//    require('./master');
-//}
-//
-//throng({
-//    workers: config.webConcurrency,
-//    lifetime: Infinity,
-//    master: startMaster,
-//    start: startWorker,
-//    grace: 4000
-//});
+function startWorker(id) {
+    console.log(`Started worker ${ id }`);
 
-require('./app');
-require('./master');
+    process.on('SIGTERM', () => {
+        console.log(`Worker ${ id } exiting...`);
+        console.log('(cleanup would happen here)');
+        process.exit();
+    });
+
+    require('./app');
+}
+
+function startMaster() {
+    require('./master');
+}
+
+throng({
+    workers: config.webConcurrency,
+    lifetime: Infinity,
+    master: startMaster,
+    start: startWorker,
+    grace: 4000
+});
