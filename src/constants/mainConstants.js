92bdc73ac6e513f9465513f45ab9e7950956ecb7
oleksijfomin <oleksij.fomin@gmail.com>
2017-01-17 14:33:37 +0200

    fix(backend/activity-list/badge): fix flow with badge.

--- src/constants/mainConstants.js
+++ src/constants/mainConstants.js
@@ -1,4 +1,5 @@
 module.exports = {
     LIST_COUNT                   : 25,
     SESSION_TTL                  : 24 * 60 * 60 * 1000, // 1 day, without rememberMe
+    REDIS_ACTIONS_TEMPLATE_STRING: 'actions'
 };
