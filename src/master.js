1c13c7fa252213b694f3ce305a1bf80a60ec8e14
oleksijfomin <oleksij.fomin@gmail.com>
2017-01-26 12:36:43 +0200

    feat(backend/push-notifications): move out redis connection from master.

--- src/master.js
+++ src/master.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose');
 const async = require('async');
 const mongo = require('./utils/mongo');
-const config = require('./config');
 const logger = require('./utils/logger');
-const RedisClient = require('./helpers/redisClient').redisClient;
 const PubNubClient = require('./stories/push-notifications/utils/pubnub');
 
 mongoose.Schemas = {}; // important thing
@@ -12,21 +10,11 @@ require('./types');
 const Scheduler = require('./helpers/scheduler');
 const scheduler = new Scheduler();
 
+mongo.on('connected', () => {
+    logger.info('Master connected to MongoDB.');
+});
+
 async.series([
-
-    (cb) => {
-        RedisClient.once('ready', () => {
-            logger.info('Master connected to Redis.');
-            cb();
-        });
-    },
-
-    (cb) => {
-        mongo.on('connected', () => {
-            logger.info('Master connected to MongoDB.');
-            cb();
-        });
-    },
 
     (cb) => {
         require('./modulesCreators')(cb);
