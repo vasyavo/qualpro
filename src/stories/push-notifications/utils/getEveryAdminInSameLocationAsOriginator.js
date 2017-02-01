ca025782c867dc1148edbb02e1017d33548c8a86
oleksijfomin <oleksij.fomin@gmail.com>
2017-02-01 20:30:10 +0200

    fix(backend/push-notifications/al-alali-branding-and-display-report): send message to every admin in action originator country and to himself.

---
+++ src/stories/push-notifications/utils/getEveryAdminInSameLocationAsOriginator.js
@@ -0,0 +1,106 @@
+const ObjectId = require('mongoose').Types.ObjectId;
+const PersonnelModel = require('./../../../types/personnel/model');
+const accessRoles = require('./../../../constants/aclRolesNames');
+
+const getEveryAdminInCountry = {
+    $or: [{
+        $setIsSubset: [['$$item.accessRole.level'], [
+            accessRoles.MASTER_ADMIN,
+            accessRoles.TRADE_MARKETER,
+        ]],
+    }, {
+        $and: [{
+            $setIsSubset: [['$$item.accessRole.level'], [
+                accessRoles.COUNTRY_ADMIN,
+                accessRoles.AREA_MANAGER,
+                accessRoles.AREA_IN_CHARGE,
+            ]],
+        }, {
+            $let: {
+                vars: {
+                    originatorCountry: {
+                        $arrayElemAt: ['$actionOriginator.country', 0],
+                    },
+                    personnelCountry: {
+                        $arrayElemAt: ['$$item.country', 0],
+                    },
+                },
+                in: {
+                    $eq: ['$$originatorCountry', '$$personnelCountry'],
+                },
+            },
+        }],
+    }],
+};
+
+/*
+ * @description Returns every admin in same location as action originator
+ * @param {Object} options
+ * @param {String} options.actionOriginator
+ * @returns {String[]}
+ * */
+
+module.exports = function * (options) {
+    const actionOriginator = ObjectId(options.actionOriginator);
+
+    const pipeline = [{
+        $lookup: {
+            from: 'accessRoles',
+            localField: 'accessRole',
+            foreignField: '_id',
+            as: 'accessRole',
+        },
+    }, {
+        $unwind: {
+            path: '$accessRole',
+        },
+    }, {
+        $group: {
+            _id: null,
+            setPersonnel: { $push: '$$ROOT' },
+        },
+    }, {
+        $project: {
+            setPersonnel: 1,
+            actionOriginator: {
+                $filter: {
+                    input: '$setPersonnel',
+                    as: 'item',
+                    cond: {
+                        $eq: ['$$item._id', actionOriginator],
+                    },
+                },
+            },
+        },
+    }, {
+        $unwind: {
+            path: '$actionOriginator',
+        },
+    }, {
+        $project: {
+            setPersonnel: 1,
+            actionOriginator: 1,
+            setAdmin: {
+                $filter: {
+                    input: '$setPersonnel',
+                    as: 'item',
+                    cond: getEveryAdminInCountry,
+                },
+            },
+        },
+    }, {
+        $project: {
+            setAdmin: '$setAdmin._id',
+        },
+    }];
+
+    const result = yield PersonnelModel.aggregate(pipeline).exec();
+    const setAdmin = result.slice()
+        .pop()
+        .setAdmin
+        .filter(objectId => objectId)
+        .map(objectId => objectId.toString());
+
+    return setAdmin;
+};
+
