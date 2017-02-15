733ce2c72535833fc6b56750437ccd44ba0bdaa1
oleksijfomin <oleksij.fomin@gmail.com>
2017-02-15 18:50:15 +0200

    fix(backend/planogram): remove index.

--- src/types/planogram/schema.js
+++ src/types/planogram/schema.js
@@ -1,40 +1,40 @@
 const mongoose = require('mongoose');
+const CONTENT_TYPES = require('./../../public/js/constants/contentType');
+
 const Schema = mongoose.Schema;
 const ObjectId = Schema.Types.ObjectId;
-const CONTENT_TYPES = require('./../../public/js/constants/contentType.js');
 
 const schema = new Schema({
-    country: { type: ObjectId, ref: CONTENT_TYPES.DOMAIN, require: true },
+    country: { type: ObjectId, ref: CONTENT_TYPES.DOMAIN, required: true },
     retailSegment: [{ type: ObjectId, ref: CONTENT_TYPES.RETAILSEGMENT }],
-    product: { type: ObjectId, ref: CONTENT_TYPES.CATEGORY, require: true },
+    product: { type: ObjectId, ref: CONTENT_TYPES.CATEGORY, required: true },
     displayType: {
-        _id : { type: String, enum: ['floorDisplayId', 'gondolaId', 'thematicStandId', 'shelfId', 'otherId']},
-        name : {
-            ar : String,
-            en : String
-        }
+        _id: { type: String, enum: ['floorDisplayId', 'gondolaId', 'thematicStandId', 'shelfId', 'otherId'] },
+        name: {
+            ar: String,
+            en: String,
+        },
     },
     configuration: {
         _id: {
-            type : ObjectId,
-            require: true
+            type: ObjectId,
+            required: true,
         },
         name: {
             type: String,
-            require: true
-        }
+            required: true,
+        },
     },
-    fileID: { type: ObjectId, ref: CONTENT_TYPES.FILES, default: null, require: true },
+    fileID: { type: ObjectId, ref: CONTENT_TYPES.FILES, default: null, required: true },
     archived: { type: Boolean, default: false },
     createdBy: {
         user: { type: ObjectId, ref: CONTENT_TYPES.PERSONNEL, default: null },
-        date: { type: Date, default: new Date() }
+        date: { type: Date, default: new Date() },
     },
     editedBy: {
         user: { type: ObjectId, ref: CONTENT_TYPES.PERSONNEL, default: null },
-        date: { type: Date, default: new Date() }
-    }
+        date: { type: Date, default: new Date() },
+    },
 }, { collection: 'planograms' });
-mongoose.connection.collection('planograms').createIndex({ 'displayType.name.en': 1, product: 1, country : 1, 'configuration._id' : 1}, { unique: true });
 
 module.exports = schema;
