bb9447e819d050d97411b697c473adb577705da6
oleksijfomin <oleksij.fomin@gmail.com>
2017-02-21 22:16:11 +0200

    feat(backend/redis): move redis client initialization to utils.

--- src/helpers/redisClient.js
+++ src/helpers/redisClient.js
@@ -1,59 +1,37 @@
-﻿'use strict';
-const config = require('./../config');
-const logger = require('./../utils/logger');
-const redis = require('redis');
-const client = redis.createClient(config.redisUrl);
+﻿const client = require('./../utils/redisClient');
 
-client.on('error', (err) => {
-    logger.error('Redis fails', err);
-});
-
-client.on('ready', () => {
-    logger.info('Redis is ready');
-});
-
-function CacheStore() {
-
-    function writeToStorage(key, value, exTime) {
-        if (exTime) {
-            client.setex([key, exTime, value]);
-
-        } else {
-            client.set([key, value]);
-
-        }
+const writeToStorage = (key, value, exTime) => {
+    if (exTime) {
+        client.setex([key, exTime, value]);
+    } else {
+        client.set([key, value]);
     }
+};
+const writeToStorageHash = (key, field, value) => {
+    client.hmset(key, field, value);
+};
+const getValuesStorageHash = (key, callback) => {
+    client.hvals(key, callback);
+};
+const getValueHashByField = (key, field, callback) => {
+    client.hget(key, field, callback);
+};
+const removeStorageHashByField = (key, field) => {
+    client.hdel(key, field);
+};
+const incrementAndGet = (key, callback) => {
+    client.incr(key, callback);
+};
+const readFromStorage = (key, callback) => {
+    client.get(key, callback);
+};
+const removeFromStorage = (key) => {
+    client.del(key, () => {});
+};
 
-    function writeToStorageHash(key, field, value) {
-        client.hmset(key, field, value);
-    }
-
-    function getValuesStorageHash(key, callback) {
-        client.hvals(key, callback);
-    }
-
-    function getValueHashByField(key, field, callback) {
-        client.hget(key, field, callback);
-    }
-
-    function removeStorageHashByField(key, field) {
-        client.hdel(key, field);
-    }
-
-    function incrementAndGet(key, callback) {
-        client.incr(key, callback);
-
-    }
-
-    function readFromStorage(key, callback) {
-        client.get(key, callback);
-    }
-
-    function removeFromStorage(key) {
-        client.del(key, redis.print);
-    }
-
-    return {
+module.exports = {
+    redisClient: client,
+    cacheStore: {
         incrementAndGet,
         writeToStorage,
         removeFromStorage,
@@ -61,13 +39,6 @@ function CacheStore() {
         writeToStorageHash,
         getValuesStorageHash,
         removeStorageHashByField,
-        getValueHashByField
-    };
-}
-
-const cacheStore = new CacheStore();
-
-module.exports = {
-    redisClient : client,
-    cacheStore
+        getValueHashByField,
+    },
 };
