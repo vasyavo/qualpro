2baf8774e2a8c866f1a60965ba1935e4ef28c210
oleksijfomin <oleksij.fomin@gmail.com>
2017-02-26 00:05:27 +0200

    fix(backend/comments): add comments on create Marketing, Al Alali Branding & Monthly display.

--- src/handlers/comment.js
+++ src/handlers/comment.js
@@ -7,6 +7,7 @@ const CompetitorBrandingModel = require('./../types/competitorBranding/model');
 const PromotionModel = require('./../types/promotion/model');
 const PromotionItemModel = require('./../types/promotionItem/model');
 const MarketingCampaignModel = require('./../types/brandingActivity/model');
+const BrandingAndDisplayModel = require('./../types/brandingAndDisplay/model');
 const MarketingCampaignItemModel = require('./../types/brandingActivityItem/model');
 const CompetitorPromotionModel = require('./../types/competitorPromotion/model');
 const ContactUsModel = require('../types/contactUs/model');
@@ -122,6 +123,10 @@ var Comment = function () {
                 case CONTENT_TYPES.BRANDING_ACTIVITY:
                     ContextModel = MarketingCampaignModel;
                     mid = ACL_MODULES.AL_ALALI_BRANDING_ACTIVITY;
+                    break;
+                case CONTENT_TYPES.BRANDING_AND_DISPLAY:
+                    ContextModel = BrandingAndDisplayModel;
+                    mid = ACL_MODULES.AL_ALALI_BRANDING_DISPLAY_REPORT;
                     break;
                 case CONTENT_TYPES.BRANDING_ACTIVITY_ITEMS:
                     ContextModel = MarketingCampaignItemModel;
