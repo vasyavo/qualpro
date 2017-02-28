d481c1ef5bf75aab095681765cafae19503519d9
oleksijfomin <oleksij.fomin@gmail.com>
2017-02-28 11:59:52 +0200

    fix(backend/login): do not expose password in response.

--- src/handlers/personnel.js
+++ src/handlers/personnel.js
@@ -1323,7 +1323,6 @@ const Personnel = function () {
             $project: {
                 email: 1,
                 mobileNumber: 1,
-                pass: 1,
                 super: 1,
                 confirmed: 1,
                 archived: 1,
