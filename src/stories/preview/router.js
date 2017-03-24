df5018e693ed042b53b7fabc279914b1e2f4466d
Edgard Tanchenec <edgar.tanchenec@thinkmobiles.com>
2017-03-24 16:39:37 +0200

    feat(backend/preview): add getForSync method

--- src/stories/preview/router.js
+++ src/stories/preview/router.js
@@ -5,6 +5,8 @@ const router = new Router();
 
 router.use(access.checkAuth);
 
+router.post('/sync', require('./routes/getForSync'));
+
 router.get('/:id([0-9a-fA-F]{24})', require('./routes/getById'));
 
 module.exports = router;
