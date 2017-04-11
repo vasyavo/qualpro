5e4219e87bd170972d7c330d26b3bfd4b728efc4
Edgard Tanchenec <edgar.tanchenec@thinkmobiles.com>
2017-04-11 12:12:33 +0300

    fix(backend/questionnaire): Fix get all

--- src/stories/push-notifications/methods/marketing/al-alali-questionnaire/prototypeUpdated.js
+++ src/stories/push-notifications/methods/marketing/al-alali-questionnaire/prototypeUpdated.js
@@ -1,7 +1,9 @@
+const ObjectId = require('mongoose').Types.ObjectId;
 const _ = require('lodash');
 const ActivityModel = require('./../../../../../types/activityList/model');
 const toString = require('./../../../../../utils/toString');
 const getEveryoneInFilledLocation = require('./../../../utils/getEveryoneInFilledLocation');
+const PersonnelModel = require('./../../../../../types/personnel/model');
 
 module.exports = function * (options) {
     const {
@@ -20,14 +22,55 @@ module.exports = function * (options) {
     const setSubRegion = Array.isArray(body.subRegion) ? body.subRegion : [];
     const setOutlet = Array.isArray(body.outlet) ? body.outlet : [];
     const setBranch = Array.isArray(body.branch) ? body.branch : [];
+    const setRetailSegment = Array.isArray(body.retailSegment) ? body.retailSegment : [];
 
-    const setEveryoneInLocation = yield getEveryoneInFilledLocation({
+    let setEveryoneInLocation = yield getEveryoneInFilledLocation({
         setCountry,
         setRegion,
         setSubRegion,
         setOutlet,
         setBranch,
-    });
+    }) || [];
+
+    const result = yield PersonnelModel.aggregate([
+        {
+            $match: {
+                _id: {
+                    $in: setEveryoneInLocation.map((personnelId) => ObjectId(personnelId)),
+                },
+            },
+        },
+        {
+            $lookup: {
+                from: 'branches',
+                localField: 'branch',
+                foreignField: '_id',
+                as: 'branch',
+            },
+        },
+        {
+            $project: {
+                retailSegments: '$branch.retailSegment',
+            },
+        },
+        {
+            $match: {
+                retailSegments: {
+                    $in: setRetailSegment,
+                },
+            },
+        },
+        {
+            $group: {
+                _id: null,
+                personnels: {
+                    $push: '$_id',
+                },
+            },
+        },
+    ]).exec();
+
+    setEveryoneInLocation = result && result[0] && result[0].personnels || [];
 
     const newActivity = new ActivityModel();
 
