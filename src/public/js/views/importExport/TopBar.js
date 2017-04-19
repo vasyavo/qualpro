b38353689c561acdd8606e4819c93c591bfc7356
ferenchuk <ivan.ferenchuk@gmail.com>
2017-04-19 17:47:22 +0300

    feat(Import/export): Added markup for import/export topbar

--- src/public/js/views/importExport/TopBar.js
+++ src/public/js/views/importExport/TopBar.js
@@ -5,6 +5,8 @@ define(function (require) {
 
     return Marionette.View.extend({
 
+        className: 'import-export-topbar',
+
         template: function () {
             return Template;
         },
