e1d2a4dcb97a95bade2648da410e12a5445f9f75
Edgard Tanchenec <edgar.tanchenec@thinkmobiles.com>
2017-04-21 15:50:07 +0300

    feat(backend/customReports): add base structure for price report

--- src/stories/customReports/router.js
+++ src/stories/customReports/router.js
@@ -2,6 +2,7 @@ const express = require('express');
 const access = require('../../helpers/access');
 
 const achievementReport = require('./achievementReport/router');
+const priceReport = require('./priceReport/router');
 
 const router = express.Router();
 const checkAuth = access.checkAuth;
@@ -9,5 +10,6 @@ const checkAuth = access.checkAuth;
 router.use(checkAuth);
 
 router.use('/achievementReport', achievementReport);
+router.use('/priceReport', priceReport);
 
 module.exports = router;
