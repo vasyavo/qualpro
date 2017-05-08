54278c5759f7c1de0a06cafadb89abdf15390f3a
petroShved <petro.shved@foxtrapp.com>
2017-05-08 10:30:25 +0300

    feat(comment): add delete

--- src/modulesCreators/roles/masterAdmin.js
+++ src/modulesCreators/roles/masterAdmin.js
@@ -691,7 +691,7 @@ module.exports = [
     {
         module: ACL_MODULES.COMMENT,
         cms: {
-            archive: false,
+            archive: true,
             edit: true,
             write: true,
             read: true
