4f95224398791f511ec9be3e7be0e05e4450c296
ivanlyah <johnnye.be@gmail.com>
2017-05-22 16:45:23 +0300

    fix(import/export): update error message (import variant) when category not found

--- src/stories/massUpload/import/item/methods/importVariant.js
+++ src/stories/massUpload/import/item/methods/importVariant.js
@@ -23,7 +23,7 @@ function* getCategoryId(name) {
     }
 
     if (!data) {
-        throw new Error(`Can not found sub-region: ${name}`);
+        throw new Error(`Can not found category: ${name}`);
     }
 
     return data;
