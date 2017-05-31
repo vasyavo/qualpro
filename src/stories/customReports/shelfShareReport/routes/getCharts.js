5424d3913e32e0a592aeaa957da8d1875d463633
petroShved <petro.shved@foxtrapp.com>
2017-05-31 18:10:25 +0300

    feat(shelfShare) add filter by time Frames

--- src/stories/customReports/shelfShareReport/routes/getCharts.js
+++ src/stories/customReports/shelfShareReport/routes/getCharts.js
@@ -41,6 +41,7 @@ module.exports = (req, res, next) => {
             CONTENT_TYPES.CATEGORY, CONTENT_TYPES.BRAND, CONTENT_TYPES.POSITION, CONTENT_TYPES.PERSONNEL,
         ];
         const pipeline = [];
+        const facetAggregation = { $facet: {} };
 
         if (timeFilter) {
             const timeFilterValidate = ajv.compile(timeFilterSchema);
@@ -55,47 +56,171 @@ module.exports = (req, res, next) => {
             }
         }
 
-        filters.forEach((filterName) => {
-            if (queryFilter[filterName] && queryFilter[filterName][0]) {
-                queryFilter[filterName] = queryFilter[filterName].map((item) => {
-                    return ObjectId(item);
+        const getPipeline = (timeFrameStage) => {
+            const pipeline = [];
+
+            filters.forEach((filterName) => {
+                if (queryFilter[filterName] && queryFilter[filterName][0]) {
+                    queryFilter[filterName] = queryFilter[filterName].map((item) => {
+                        return ObjectId(item);
+                    });
+                }
+            });
+
+            locationFiler(pipeline, personnel, queryFilter);
+
+            const $generalMatch = generalFiler([CONTENT_TYPES.RETAILSEGMENT, CONTENT_TYPES.OUTLET, CONTENT_TYPES.CATEGORY], queryFilter, personnel);
+
+            if (queryFilter.brand && queryFilter.brand.length) {
+                $generalMatch.$and.push({
+                    'brands.brand': {
+                        $in: queryFilter.brand,
+                    },
                 });
             }
+
+            if (queryFilter[CONTENT_TYPES.PERSONNEL] && queryFilter[CONTENT_TYPES.PERSONNEL].length) {
+                $generalMatch.$and.push({
+                    'createdBy.user': {
+                        $in: queryFilter[CONTENT_TYPES.PERSONNEL],
+                    },
+                });
+            }
+
+            if ($generalMatch.$and.length) {
+                pipeline.push({
+                    $match: $generalMatch,
+                });
+            }
+
+            pipeline.push(timeFrameStage);
+
+            pipeline.push({
+                $lookup: {
+                    from: 'personnels',
+                    localField: 'createdBy.user',
+                    foreignField: '_id',
+                    as: 'createdBy.user',
+                },
+            });
+
+            pipeline.push({
+                $project: {
+                    _id: 1,
+                    category: 1,
+                    brands: 1,
+                    createdBy: {
+                        user: {
+                            $let: {
+                                vars: {
+                                    user: { $arrayElemAt: ['$createdBy.user', 0] },
+                                },
+                                in: {
+                                    _id: '$$user._id',
+                                    position: '$$user.position',
+                                },
+                            },
+                        },
+                        date: 1,
+                    },
+                },
+            });
+
+            if (queryFilter[CONTENT_TYPES.POSITION] && queryFilter[CONTENT_TYPES.POSITION].length) {
+                pipeline.push({
+                    $match: {
+                        'createdBy.user.position': {
+                            $in: queryFilter[CONTENT_TYPES.POSITION],
+                        },
+                    },
+                });
+            }
+
+            pipeline.push({
+                $unwind: '$brands',
+            });
+
+            pipeline.push({
+                $lookup: {
+                    from: 'brands',
+                    localField: 'brands.brand',
+                    foreignField: '_id',
+                    as: 'brand',
+                },
+            });
+
+            pipeline.push({
+                $project: {
+                    _id: 1,
+                    brands: {
+                        _id: '$brands.brand',
+                        percent: 1,
+                        name: {
+                            $let: {
+                                vars: {
+                                    brand: { $arrayElemAt: ['$brand', 0] },
+                                },
+                                in: '$$brand.name',
+                            },
+                        },
+                    },
+                },
+            });
+
+            pipeline.push({
+                $group: {
+                    _id: '$brands._id',
+                    percent: { $avg: '$brands.percent' },
+                    name: { $first: '$brands.name' },
+                },
+            });
+
+            pipeline.push({
+                $group: {
+                    _id: null,
+                    data: {
+                        $addToSet: '$percent',
+                    },
+                    labels: { $addToSet: '$name' },
+                },
+            });
+
+            pipeline.push({
+                $project: {
+                    barChart: {
+                        data: '$data',
+                        labels: '$labels',
+                    },
+                },
+            });
+
+            return pipeline;
+        };
+
+        timeFilter.forEach((timeFrame) => {
+            const fromToTimeFrame = `${timeFrame.from} - ${timeFrame.to}`;
+            const match = {
+                $match: {
+                    $and: [
+                        {
+                            'createdBy.date': { $gt: moment(timeFrame.from, 'MM/DD/YYYY')._d },
+                        },
+                        {
+                            'createdBy.date': { $lt: moment(timeFrame.to, 'MM/DD/YYYY')._d },
+                        },
+                    ],
+                },
+            };
+            facetAggregation.$facet[fromToTimeFrame] = getPipeline(match);
         });
 
-        locationFiler(pipeline, personnel, queryFilter);
+        const $timeMatch = { $match: {} };
 
-        const $generalMatch = generalFiler([CONTENT_TYPES.RETAILSEGMENT, CONTENT_TYPES.OUTLET, CONTENT_TYPES.CATEGORY], queryFilter, personnel);
-
-        if (queryFilter.brand && queryFilter.brand.length) {
-            $generalMatch.$and.push({
-                'brands.brand': {
-                    $in: queryFilter.brand,
-                },
-            });
-        }
-
-        if (queryFilter[CONTENT_TYPES.PERSONNEL] && queryFilter[CONTENT_TYPES.PERSONNEL].length) {
-            $generalMatch.$and.push({
-                'createdBy.user': {
-                    $in: queryFilter[CONTENT_TYPES.PERSONNEL],
-                },
-            });
-        }
-
-        if ($generalMatch.$and.length) {
-            pipeline.push({
-                $match: $generalMatch,
-            });
-        }
-
-        const $timeMatch = {};
-
-        $timeMatch.$or = [];
+        $timeMatch.$match.$or = [];
 
         if (timeFilter) {
             timeFilter.map((frame) => {
-                $timeMatch.$or.push({
+                $timeMatch.$match.$or.push({
                     $and: [
                         {
                             'createdBy.date': { $gt: moment(frame.from, 'MM/DD/YYYY')._d },
@@ -109,110 +234,11 @@ module.exports = (req, res, next) => {
             });
         }
 
-        if ($timeMatch.$or.length) {
-            pipeline.push({
-                $match: $timeMatch,
-            });
+        if ($timeMatch.$match.$or) {
+            facetAggregation.$facet.timePeriod = getPipeline($timeMatch);
         }
 
-        pipeline.push({
-            $lookup: {
-                from: 'personnels',
-                localField: 'createdBy.user',
-                foreignField: '_id',
-                as: 'createdBy.user',
-            },
-        });
-
-        pipeline.push({
-            $project: {
-                _id: 1,
-                category: 1,
-                brands: 1,
-                createdBy: {
-                    user: {
-                        $let: {
-                            vars: {
-                                user: { $arrayElemAt: ['$createdBy.user', 0] },
-                            },
-                            in: {
-                                _id: '$$user._id',
-                                position: '$$user.position',
-                            },
-                        },
-                    },
-                    date: 1,
-                },
-            },
-        });
-
-        if (queryFilter[CONTENT_TYPES.POSITION] && queryFilter[CONTENT_TYPES.POSITION].length) {
-            pipeline.push({
-                $match: {
-                    'createdBy.user.position': {
-                        $in: queryFilter[CONTENT_TYPES.POSITION],
-                    },
-                },
-            });
-        }
-
-        pipeline.push({
-            $unwind: '$brands',
-        });
-
-        pipeline.push({
-            $lookup: {
-                from: 'brands',
-                localField: 'brands.brand',
-                foreignField: '_id',
-                as: 'brand',
-            },
-        });
-
-        pipeline.push({
-            $project: {
-                _id: 1,
-                brands: {
-                    _id: '$brands.brand',
-                    percent: 1,
-                    name: {
-                        $let: {
-                            vars: {
-                                brand: { $arrayElemAt: ['$brand', 0] },
-                            },
-                            in: '$$brand.name',
-                        },
-                    },
-                },
-            },
-        });
-
-        pipeline.push({
-            $group: {
-                _id: '$brands._id',
-                percent: { $avg: '$brands.percent' },
-                name: { $first: '$brands.name' },
-            },
-        });
-
-        pipeline.push({
-            $group: {
-                _id: null,
-                data: {
-                    $addToSet: '$percent',
-                },
-                labels: { $addToSet: '$name' },
-            },
-        });
-
-        pipeline.push({
-            $project: {
-                barChart: {
-                    data: '$data',
-                    labels: '$labels',
-                },
-            },
-        });
+        pipeline.push(facetAggregation);
 
         ShelfShareModel.aggregate(pipeline)
             .allowDiskUse(true)
@@ -231,17 +257,32 @@ module.exports = (req, res, next) => {
             return next(err);
         }
 
-        const response = result[0];
+        const responseData = result[0];
+        const response = { barChart: { datasets: [] } };
 
-        if (response) {
+        if (responseData) {
+            const keys = Object.keys(responseData);
             response.barChart = {
-                labels: response.barChart.labels,
-                datasets: [{
-                    data: response.barChart.data.map(percent => parseFloat(percent).toFixed(2)),
-                }],
+                datasets: [],
             };
-
-            response.pieChart = response.barChart;
+            keys.forEach((key) => {
+                if (responseData[key]) {
+                    const chartData = responseData[key][0];
+                    if (key === 'timePeriod') {
+                        response.pieChart = {
+                            labels: chartData.barChart.labels,
+                            datasets: [{
+                                data: chartData.barChart.data.map(percent => parseFloat(percent).toFixed(2)),
+                            }],
+                        };
+                    } else {
+                        response.barChart.datasets.push({
+                            data: chartData.barChart.data.map(percent => parseFloat(percent).toFixed(2)),
+                        });
+                        response.barChart.labels = chartData.barChart.labels;
+                    }
+                }
+            });
 
             return res.status(200).send(response);
         }
