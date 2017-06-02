e5c76e1bfb1f0eee5e76bba3587019864e8ca4a3
petroShved <petro.shved@foxtrapp.com>
2017-06-02 23:35:20 +0300

    feat(shelfShare) add export

--- src/stories/customReports/shelfShareReport/router.js
+++ src/stories/customReports/shelfShareReport/router.js
@@ -4,6 +4,7 @@ const getAll = require('./routes/getAll');
 const getDetails = require('./routes/getDetails');
 const getCharts = require('./routes/getCharts');
 const getFilters = require('./routes/getFilters');
+const XMLExport = require('./routes/XMLExport');
 
 const router = express.Router();
 
@@ -11,5 +12,6 @@ router.get('/', getAll);
 router.get('/details', getDetails);
 router.get('/charts', getCharts);
 router.get('/filters', getFilters);
+router.get('/export', XMLExport);
 
 module.exports = router;
