a85ecc55a7b3e1e147a39bb8d6e5512effc44c60
yaroslav.fedyshyn <fedyshyn.y@gmail.com>
2017-06-02 17:50:05 +0300

    fix(export): phantomjs

---
+++ src/utils/conversionHtmlToXlsx.js
@@ -0,0 +1,15 @@
+const conversion = require('html-to-xlsx')({
+    /* number of allocated phantomjs processes */
+    numberOfWorkers: 2,
+    /* timeout in ms for html conversion, when the timeout is reached, the phantom process is recycled */
+    timeout: 5000,
+    /* directory where are stored temporary html and pdf files, use something like npm package reaper to clean this up */
+    tmpDir: "os/tmpdir",
+    /* optional port range where to start phantomjs server */
+    portLeftBoundary: 3004,
+    portRightBoundary: 3006,
+    /* optional hostname where to start phantomjs server */
+    host: '127.0.0.1'
+});
+
+module.exports = conversion;
