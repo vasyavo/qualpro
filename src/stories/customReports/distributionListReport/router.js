a743f9e50fc61c5f73e767670a72f855423b0522
Edgard Tanchenec <edgar.tanchenec@thinkmobiles.com>
2017-06-12 15:39:33 +0300

    feat(backend/DistributionListReport): retrieve data for filters

--- src/stories/customReports/distributionListReport/router.js
+++ src/stories/customReports/distributionListReport/router.js
@@ -1,9 +1,12 @@
 const express = require('express');
 
 const getAll = require('./routes/getAll');
+const getFilters = require('./routes/getFilters');
+
 
 const router = express.Router();
 
 router.get('/', getAll);
+router.get('/filters', getFilters);
 
 module.exports = router;
