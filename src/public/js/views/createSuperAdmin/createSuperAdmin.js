481fca340c01b8cdbb0711e5f8e4ef14a7938d80
yaroslav.fedyshyn <fedyshyn.y@gmail.com>
2017-07-12 10:36:19 +0300

    refactor(views/createSuperAdmin): migrate files to webpack pattern.

--- src/public/js/views/createSuperAdmin/createSuperAdmin.js
+++ src/public/js/views/createSuperAdmin/createSuperAdmin.js
@@ -1,59 +1,53 @@
-define([
-    'backbone',
-    'Underscore',
-    'jQuery',
-    'text!templates/createSuperAdmin/createSuperAdmin.html',
-    'dataService',
-    'constants/errorMessages'
-], function (Backbone, _, $, createSuperAdminTemplate, dataService, ERROR_MESSAGES) {
+var $ = require('jQuery');
+var _ = require('underscore');
+var Backbone = require('backbone');
+var dataService = require('../../dataService');
+var createSuperAdminTemplate = require('../../../templates/createSuperAdmin/createSuperAdmin.html');
+var ERROR_MESSAGES = require('../../constants/errorMessages');
 
-    var createSuperAdminView = Backbone.View.extend({
-        el      : '#wrapper',
-        template: _.template(createSuperAdminTemplate),
+module.exports = Backbone.View.extend({
+    el      : '#wrapper',
+    template: _.template(createSuperAdminTemplate),
 
-        initialize: function () {
-            this.render();
-        },
+    initialize: function () {
+        this.render();
+    },
 
-        events: {
-            'submit #loginForm': 'createSuperAdmin'
-        },
+    events: {
+        'submit #loginForm': 'createSuperAdmin'
+    },
 
-        createSuperAdmin: function (e) {
-            var $el = $(e.target);
-            var repeatPassword = $el.find('input[name="repeatPassword"]').val();
-            var data = {
-                email: $el.find('input[name="email"]').val(),
-                pass : $el.find('input[name="password"]').val()
-            };
-            var currentUser = App.currentUser;
-            var currentLanguage = currentUser && currentUser.currentLanguage || 'en';
+    createSuperAdmin: function (e) {
+        var $el = $(e.target);
+        var repeatPassword = $el.find('input[name="repeatPassword"]').val();
+        var data = {
+            email: $el.find('input[name="email"]').val(),
+            pass : $el.find('input[name="password"]').val()
+        };
+        var currentUser = App.currentUser;
+        var currentLanguage = currentUser && currentUser.currentLanguage || 'en';
 
-            e.preventDefault();
+        e.preventDefault();
 
-            if (!data.email || !(data.pass === repeatPassword) || !data.pass) {
-                return App.render({type: 'error', message: ERROR_MESSAGES.notValidData[currentLanguage]});
+        if (!data.email || !(data.pass === repeatPassword) || !data.pass) {
+            return App.render({type: 'error', message: ERROR_MESSAGES.notValidData[currentLanguage]});
+        }
+
+        dataService.postData('/personnel/createSuper', data, function (err, res) {
+            if (err) {
+                return App.render(err);
             }
 
-            dataService.postData('/personnel/createSuper', data, function (err, res) {
-                if (err) {
-                    return App.render(err);
-                }
+            App.render({type: 'notification', message: ERROR_MESSAGES.checkYourEmail[currentLanguage]});
+        });
+    },
 
-                App.render({type: 'notification', message: ERROR_MESSAGES.checkYourEmail[currentLanguage]});
-            });
-        },
+    render: function () {
+        var thisEl = this.$el;
+        thisEl.html(this.template());
 
-        render: function () {
-            var thisEl = this.$el;
-            thisEl.html(this.template());
+        this.$errrorHandler = $('#errorHandler');
 
-            this.$errrorHandler = $('#errorHandler');
-
-            return this;
-        }
-    });
-
-    return createSuperAdminView;
-
+        return this;
+    }
 });
