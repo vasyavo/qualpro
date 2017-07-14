7124d60a3f2788b21730046b1abc51484fa6f0d0
yaroslav.fedyshyn <fedyshyn.y@gmail.com>
2017-07-14 11:34:09 +0300

    refactor(views/retailSegment): migrate files to webpack pattern.

--- src/public/js/views/retailSegment/list/listView.js
+++ src/public/js/views/retailSegment/list/listView.js
@@ -1,121 +1,117 @@
-define(function(require) {
-    var _ = require('underscore');
-    var $ = require('jQuery');
-    var paginator = require('views/paginator');
-    var ListItemsView = require('views/retailSegment/list/listItemsView');
-    var PreView = require('views/domain/preView/preView');
-    var CreateView = require('views/domain/createView');
-    var EditView = require('views/domain/editView');
-    var headerTemplate = require('text!templates/retailSegment/list/listHeader.html');
-    var newRow = require('text!templates/domain/newRow.html');
-    var BadgeStore = require('services/badgeStore');
+var _ = require('underscore');
+var $ = require('jQuery');
+var paginator = require('../../../views/paginator');
+var ListItemsView = require('../../../views/retailSegment/list/listItemsView');
+var PreView = require('../../../views/domain/preView/preView');
+var CreateView = require('../../../views/domain/createView');
+var EditView = require('../../../views/domain/editView');
+var headerTemplate = require('../../../../templates/retailSegment/list/listHeader.html');
+var newRow = require('../../../../templates/domain/newRow.html');
+var BadgeStore = require('../../../services/badgeStore');
 
-    var View = paginator.extend({
-        el         : '#contentHolder',
-        contentType: 'retailSegment',
-        viewType   : 'list',
-        CreateView : CreateView,
-        EditView   : EditView,
+module.exports = paginator.extend({
+    el         : '#contentHolder',
+    contentType: 'retailSegment',
+    viewType   : 'list',
+    CreateView : CreateView,
+    EditView   : EditView,
 
-        template   : _.template(headerTemplate),
-        templateNew: _.template(newRow),
+    template   : _.template(headerTemplate),
+    templateNew: _.template(newRow),
 
-        events: {
-            'click .checkboxLabel'        : 'checked',
-            'click input[type="checkbox"]': 'inputClick',
-            'click .listRow:not(label)'   : 'incClicks'
-        },
+    events: {
+        'click .checkboxLabel'        : 'checked',
+        'click input[type="checkbox"]': 'inputClick',
+        'click .listRow:not(label)'   : 'incClicks'
+    },
 
-        initialize: function (options) {
-            this.translation = options.translation;
-            this.filter = options.filter;
-            this.collection = options.collection;
-            this.defaultItemsNumber = this.collection.pageSize;
-            this.listLength = this.collection.totalRecords;
-            this.page = options.collection.page;
-            this.singleSelect = options.singleSelect;
+    initialize: function (options) {
+        this.translation = options.translation;
+        this.filter = options.filter;
+        this.collection = options.collection;
+        this.defaultItemsNumber = this.collection.pageSize;
+        this.listLength = this.collection.totalRecords;
+        this.page = options.collection.page;
+        this.singleSelect = options.singleSelect;
 
-            BadgeStore.cleanupTradeChannel();
+        BadgeStore.cleanupTradeChannel();
 
-            this.makeRender(options);
-        },
+        this.makeRender(options);
+    },
 
-        listRowClick: function (e) {
-            var $targetEl = $(e.target);
-            var $targetRow = $targetEl.closest('tr');
-            //var name = $targetRow.attr('data-name');
-            var id = $targetRow.attr('data-id');
+    listRowClick: function (e) {
+        var $targetEl = $(e.target);
+        var $targetRow = $targetEl.closest('tr');
+        //var name = $targetRow.attr('data-name');
+        var id = $targetRow.attr('data-id');
 
-            var model = this.collection.get(id);
+        var model = this.collection.get(id);
 
-            new PreView({contentType: this.contentType, model: model, translation: this.translation});
-        },
+        new PreView({contentType: this.contentType, model: model, translation: this.translation});
+    },
 
-        showMoreContent: function (newModels) {
-            var $holder = this.$el;
-            var itemView;
-            var show = this.checkShowAccess();
+    showMoreContent: function (newModels) {
+        var $holder = this.$el;
+        var itemView;
+        var show = this.checkShowAccess();
 
-            $holder.find('.listTable').empty();
+        $holder.find('.listTable').empty();
 
-            this.trigger('hideActionDd');
+        this.trigger('hideActionDd');
 
-            itemView = new ListItemsView({
-                el         : this.$itemsEl,
-                collection : newModels,
-                contentType: this.contentType,
-                tabName    : this.tabName,
-                show       : show,
-                translation: this.translation
-            });
+        itemView = new ListItemsView({
+            el         : this.$itemsEl,
+            collection : newModels,
+            contentType: this.contentType,
+            tabName    : this.tabName,
+            show       : show,
+            translation: this.translation
+        });
 
-            $holder.append(itemView.render());
-            itemView.undelegateEvents();
+        $holder.append(itemView.render());
+        itemView.undelegateEvents();
 
-            $holder.find('#checkAll').prop('checked', false);
-        },
+        $holder.find('#checkAll').prop('checked', false);
+    },
 
-        render: function () {
-            var $currentEl = this.$el;
-            var show = this.checkShowAccess();
+    render: function () {
+        var $currentEl = this.$el;
+        var show = this.checkShowAccess();
 
-            $currentEl.html('');
-            $currentEl.append(this.template({
-                show       : show,
-                translation: this.translation
-            }));
+        $currentEl.html('');
+        $currentEl.append(this.template({
+            show       : show,
+            translation: this.translation
+        }));
 
-            this.$itemsEl = $currentEl.find('.listTable');
+        this.$itemsEl = $currentEl.find('.listTable');
 
-            $currentEl.append(new ListItemsView({
-                el         : this.$itemsEl,
-                collection : this.collection,
-                tabName    : this.tabName,
-                show       : show,
-                translation: this.translation
-            }).render());
+        $currentEl.append(new ListItemsView({
+            el         : this.$itemsEl,
+            collection : this.collection,
+            tabName    : this.tabName,
+            show       : show,
+            translation: this.translation
+        }).render());
 
-            return this;
-        },
+        return this;
+    },
 
-        checkShowAccess: function () {
-            var accessLevel = App.currentUser.accessRole.level;
-            var contentType = this.contentType;
+    checkShowAccess: function () {
+        var accessLevel = App.currentUser.accessRole.level;
+        var contentType = this.contentType;
 
-            var checkBoxesObject = {
-                2: ['country'],
-                9: ['country'],
-                3: ['country', 'region', 'subRegion', 'retailSegment', 'outlet', 'branch'],
-                4: ['country', 'region', 'subRegion', 'retailSegment', 'outlet', 'branch']
-            };
+        var checkBoxesObject = {
+            2: ['country'],
+            9: ['country'],
+            3: ['country', 'region', 'subRegion', 'retailSegment', 'outlet', 'branch'],
+            4: ['country', 'region', 'subRegion', 'retailSegment', 'outlet', 'branch']
+        };
 
-            if (checkBoxesObject.hasOwnProperty(accessLevel) && checkBoxesObject[accessLevel].indexOf(contentType) !== -1) {
-                return false;
-            }
-            return true;
+        if (checkBoxesObject.hasOwnProperty(accessLevel) && checkBoxesObject[accessLevel].indexOf(contentType) !== -1) {
+            return false;
         }
+        return true;
+    }
 
-    });
-
-    return View;
 });
--- src/public/js/views/retailSegment/thumbnails/thumbnailsView.js
+++ src/public/js/views/retailSegment/thumbnails/thumbnailsView.js
@@ -1,122 +1,116 @@
-define(function (require) {
-    var _ = require('underscore');
-    var $ = require('jQuery');
-    var thumbnailsTemplate = require('text!templates/retailSegment/thumbnails/thumbnailsView.html');
-    var thumbnails = require('views/thumbnails');
-    var dataService = require('dataService');
-    var createView = require('views/domain/createView');
-    var EditView = require('views/domain/editView');
-    var PreView = require('views/domain/preView/preView');
-    var newThumbnail = require('text!templates/domain/newThumbnail.html');
-    var BadgeStore = require('services/badgeStore');
+var _ = require('underscore');
+var $ = require('jQuery');
+var thumbnailsTemplate = require('../../../../templates/retailSegment/thumbnails/thumbnailsView.html');
+var thumbnails = require('../../../views/thumbnails');
+var createView = require('../../../views/domain/createView');
+var EditView = require('../../../views/domain/editView');
+var PreView = require('../../../views/domain/preView/preView');
+var newThumbnail = require('../../../../templates/domain/newThumbnail.html');
+var BadgeStore = require('../../../services/badgeStore');
 
-    var View = thumbnails.extend({
-        el         : '#contentHolder',
-        viewType   : 'thumbnails',
-        template   : _.template(thumbnailsTemplate),
-        templateNew: _.template(newThumbnail),
+module.exports = thumbnails.extend({
+    el         : '#contentHolder',
+    viewType   : 'thumbnails',
+    template   : _.template(thumbnailsTemplate),
+    templateNew: _.template(newThumbnail),
 
-        CreateView: createView,
-        EditView  : EditView,
+    CreateView: createView,
+    EditView  : EditView,
 
-        view        : null,
-        breadcrumbs : null,
-        contentType : 'retailSegment',
-        $mainContent: null,
-        parentId    : null,
+    view        : null,
+    breadcrumbs : null,
+    contentType : 'retailSegment',
+    $mainContent: null,
+    parentId    : null,
 
-        events: {
-            'click .checkboxLabel'             : 'checked',
-            'click input[type="checkbox"]'     : 'inputClick',
-            'click .thumbnail:not(label,input)': 'incClicks'
-        },
+    events: {
+        'click .checkboxLabel'             : 'checked',
+        'click input[type="checkbox"]'     : 'inputClick',
+        'click .thumbnail:not(label,input)': 'incClicks'
+    },
 
-        initialize: function (options) {
-            this.filter = options.filter;
-            this.ContentCollection = options.ContentCollection;
-            this.collection = options.collection;
-            this.defaultItemsNumber = this.collection.pageSize;
-            this.listLength = this.collection.totalRecords;
-            this.page = options.collection.page;
+    initialize: function (options) {
+        this.filter = options.filter;
+        this.ContentCollection = options.ContentCollection;
+        this.collection = options.collection;
+        this.defaultItemsNumber = this.collection.pageSize;
+        this.listLength = this.collection.totalRecords;
+        this.page = options.collection.page;
 
-            BadgeStore.cleanupTradeChannel();
+        BadgeStore.cleanupTradeChannel();
 
-            this.makeRender(options);
-        },
+        this.makeRender(options);
+    },
 
-        render: function () {
-            var $currentEl = this.$el;
-            var self = this;
-            var $holder;
+    render: function () {
+        var $currentEl = this.$el;
+        var self = this;
+        var $holder;
 
-            var show = this.checkShowAccess();
+        var show = this.checkShowAccess();
 
-            $currentEl.html('');
+        $currentEl.html('');
 
-            $currentEl.append('<div class="thumbnailsHolder scrollable"><div class="thumbnailsItems"></div></div>');
+        $currentEl.append('<div class="thumbnailsHolder scrollable"><div class="thumbnailsItems"></div></div>');
 
-            $holder = $currentEl.find('.thumbnailsItems');
+        $holder = $currentEl.find('.thumbnailsItems');
 
-            $holder.append(this.template({
-                collection: self.collection.toJSON(),
-                tabName   : this.tabName,
-                show      : show
-            }));
+        $holder.append(this.template({
+            collection: self.collection.toJSON(),
+            tabName   : this.tabName,
+            show      : show
+        }));
 
-            return this;
-        },
+        return this;
+    },
 
-        showPreview: function (e) {
-            var targetEl = $(e.target);
-            var targetDivContainer = targetEl.closest('.thumbnail.retailSegment');
-            var id = targetDivContainer.attr('data-id');
+    showPreview: function (e) {
+        var targetEl = $(e.target);
+        var targetDivContainer = targetEl.closest('.thumbnail.retailSegment');
+        var id = targetDivContainer.attr('data-id');
 
-            var model = this.collection.get(id);
+        var model = this.collection.get(id);
 
-            new PreView({contentType: this.contentType, model: model, translation: this.translation});
-        },
+        new PreView({contentType: this.contentType, model: model, translation: this.translation});
+    },
 
-        showMoreContent: function (newModels) {
-            var $currentEl = this.$el;
-            var $holder = $currentEl.find('.thumbnailsItems');
-            var jsonCollection = newModels.toJSON();
-            var show = this.checkShowAccess();
+    showMoreContent: function (newModels) {
+        var $currentEl = this.$el;
+        var $holder = $currentEl.find('.thumbnailsItems');
+        var jsonCollection = newModels.toJSON();
+        var show = this.checkShowAccess();
 
-            this.pageAnimation(this.collection.direction, $holder);
+        this.pageAnimation(this.collection.direction, $holder);
 
-            this.trigger('hideActionDd');
-            this.trigger('unCheckSelectAll');
+        this.trigger('hideActionDd');
+        this.trigger('unCheckSelectAll');
 
-            jsonCollection.contentType = this.contentType;
+        jsonCollection.contentType = this.contentType;
 
-            $holder.empty();
-            $holder.html(this.template({
-                collection: jsonCollection,
-                tabName   : this.tabName,
-                show      : show
-            }));
+        $holder.empty();
+        $holder.html(this.template({
+            collection: jsonCollection,
+            tabName   : this.tabName,
+            show      : show
+        }));
 
-            $holder.find('#checkAll').prop('checked', false);
-        },
+        $holder.find('#checkAll').prop('checked', false);
+    },
 
-        checkShowAccess: function () {
-            var accessLevel = App.currentUser.accessRole.level;
-            var contentType = this.contentType;
+    checkShowAccess: function () {
+        var accessLevel = App.currentUser.accessRole.level;
+        var contentType = this.contentType;
 
-            var checkBoxesObject = {
-                2: ['country'],
-                9: ['country'],
-                3: ['country', 'region', 'subRegion', 'retailSegment', 'outlet', 'branch'],
-                4: ['country', 'region', 'subRegion', 'retailSegment', 'outlet', 'branch']
-            };
+        var checkBoxesObject = {
+            2: ['country'],
+            9: ['country'],
+            3: ['country', 'region', 'subRegion', 'retailSegment', 'outlet', 'branch'],
+            4: ['country', 'region', 'subRegion', 'retailSegment', 'outlet', 'branch']
+        };
 
-            if (checkBoxesObject.hasOwnProperty(accessLevel) && checkBoxesObject[accessLevel].indexOf(contentType) !== -1) {
-                return false;
-            }
-            return true;
+        if (checkBoxesObject.hasOwnProperty(accessLevel) && checkBoxesObject[accessLevel].indexOf(contentType) !== -1) {
+            return false;
         }
-    });
-
-    return View;
+        return true;
+    }
 });
-
