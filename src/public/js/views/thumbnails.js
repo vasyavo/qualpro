118e62d0327f6e5b44f9a7f97c805af03c6a9760
yaroslav.fedyshyn <fedyshyn.y@gmail.com>
2017-07-14 11:59:31 +0300

    refactor(views/thumbnails.js): migrate files to webpack pattern.

--- src/public/js/views/thumbnails.js
+++ src/public/js/views/thumbnails.js
@@ -1,166 +1,162 @@
-define([
-    'underscore',
-    'views/paginator'
-], function (_, paginator) {
-    var View = paginator.extend({
+var _ = require('underscore');
+var paginator = require('../views/paginator');
 
-        thumbnailClicks: 0,
+module.exports = paginator.extend({
 
-        incClicks: function (e) {
-            this.thumbnailClicks += 1;
-            this.runClickThumbnail(e);
-        },
+    thumbnailClicks: 0,
 
-        clickThumbnail: function (e) {
-            var clicks = this.thumbnailClicks;
+    incClicks: function (e) {
+        this.thumbnailClicks += 1;
+        this.runClickThumbnail(e);
+    },
 
-            this.thumbnailClicks = 0;
+    clickThumbnail: function (e) {
+        var clicks = this.thumbnailClicks;
 
-            if (clicks == 1) {
-                this.checkThumbnail(e);
-            } else {
-                this.showPreview(e);
-            }
-        },
+        this.thumbnailClicks = 0;
 
-        createItem: function () {
-            var contentType = this.contentType;
-            var viewType = this.viewType;
-            var parentId = this.parentId;
-            var modelUrl = 'models/' + contentType;
-            var self = this;
-            var CreateView = this.CreateView;
+        if (clicks == 1) {
+            this.checkThumbnail(e);
+        } else {
+            this.showPreview(e);
+        }
+    },
 
-            require([modelUrl], function (Model) {
-                var createView = new CreateView({
-                    Model      : Model,
-                    contentType: contentType,
-                    viewType   : viewType,
-                    parentId   : parentId,
-                    translation: self.translation
-                });
+    createItem: function () {
+        var contentType = this.contentType;
+        var viewType = this.viewType;
+        var parentId = this.parentId;
+        var modelUrl = 'models/' + contentType;
+        var self = this;
+        var CreateView = this.CreateView;
 
-                createView.on('itemSaved', function () {
-                    self.collection.getPage(1);
-                });
-
-                createView.on('modelSaved', function (model) {
-                    self.addReplaceRow(model);
-                });
-            });
-        },
-
-        editItem: function (id) {
-            var contentType = this.contentType;
-            var viewType = this.viewType;
-            var parentId = this.parentId;
-            var curentId = id || this.$el.find('input[type="checkbox"]:checked').attr('id');
-            var model = this.collection.get(curentId);
-            var self = this;
-
-            var editView = new this.EditView({
-                model      : model,
+        require([modelUrl], function (Model) {
+            var createView = new CreateView({
+                Model      : Model,
                 contentType: contentType,
                 viewType   : viewType,
                 parentId   : parentId,
-                translation: this.translation
+                translation: self.translation
             });
 
-            editView.on('itemArchived', function () {
+            createView.on('itemSaved', function () {
                 self.collection.getPage(1);
             });
 
-            editView.on('modelSaved', function (model) {
-
-                if (App.currentUser._id === model.get('_id')){
-                    App.currentUser = model.toJSON();
-                    self.trigger('renderCurrentUserInfo');
-                }
-
+            createView.on('modelSaved', function (model) {
                 self.addReplaceRow(model);
             });
-        },
+        });
+    },
 
-        addReplaceRow: function (model) {
-            var $curEl = this.$el;
-            var $thumbnailsContainer = $curEl.find('.thumbnailsItems');
-            var id = model.get('_id');
-            var $thumbnails = $thumbnailsContainer.find('.thumbnail[data-id="' + id + '"]');
-            var modelJSON = model.toJSON();
+    editItem: function (id) {
+        var contentType = this.contentType;
+        var viewType = this.viewType;
+        var parentId = this.parentId;
+        var curentId = id || this.$el.find('input[type="checkbox"]:checked').attr('id');
+        var model = this.collection.get(curentId);
+        var self = this;
 
-            var formString = this.templateNew({model: modelJSON});
+        var editView = new this.EditView({
+            model      : model,
+            contentType: contentType,
+            viewType   : viewType,
+            parentId   : parentId,
+            translation: this.translation
+        });
 
-            if ($thumbnails.length) {
-                $thumbnails.replaceWith(formString);
-                $curEl.find('.rating').barrating({readonly: true});
-                if (this.preView) {
-                    this.preView.trigger('updatePreview', model);
-                }
-                this.collection.add(modelJSON, {merge: true});
-            } else {
-                $curEl.find('.noData').remove();
-                this.collection.add(modelJSON, {remove: false});
-                this.trigger('pagination',{
-                    length     : ++this.collection.totalRecords,
-                    currentPage: this.collection.currentPage,
-                    itemsNumber: ++this.collection.pageSize
-                });
-                $thumbnailsContainer.prepend(this.templateNew({
-                    model: modelJSON
-                }));
+        editView.on('itemArchived', function () {
+            self.collection.getPage(1);
+        });
+
+        editView.on('modelSaved', function (model) {
+
+            if (App.currentUser._id === model.get('_id')){
+                App.currentUser = model.toJSON();
+                self.trigger('renderCurrentUserInfo');
             }
 
-            this.trigger('hideActionDd');
-        },
+            self.addReplaceRow(model);
+        });
+    },
 
-        checkThumbnail: function (e) {
-            var $targetEl = $(e.target);
-            var $targetDivContainer = $targetEl.closest(".thumbnail");
-            var $checkbox = $targetDivContainer.find('input[type="checkbox"]');
-            var checked = $checkbox.prop('checked');
+    addReplaceRow: function (model) {
+        var $curEl = this.$el;
+        var $thumbnailsContainer = $curEl.find('.thumbnailsItems');
+        var id = model.get('_id');
+        var $thumbnails = $thumbnailsContainer.find('.thumbnail[data-id="' + id + '"]');
+        var modelJSON = model.toJSON();
 
-            $checkbox.prop('checked', !checked);
+        var formString = this.templateNew({model: modelJSON});
 
-            this.inputClick(e);
-        },
-
-        showFilteredPage: function (filter) {
-            var itemsNumber = $("#itemsNumber").text();
-
-            this.filter = filter;
-
-            this.startTime = new Date();
-            this.newCollection = false;
-
-            $("#top-bar-deleteBtn").hide();
-            $('#checkAll').prop('checked', false);
-
-            this.changeLocationHash(1, itemsNumber, filter);
-            this.collection.firstPage({count: itemsNumber, filter: filter});
-        },
-
-        showMoreContent: function (newModels) {
-            var $currentEl = this.$el;
-            var $holder = $currentEl.find('.thumbnailsItems');
-
-            this.pageAnimation(this.collection.direction, $holder);
-
-            $holder.empty();
-            $holder.html(this.template({
-                collection: newModels.toJSON()
+        if ($thumbnails.length) {
+            $thumbnails.replaceWith(formString);
+            $curEl.find('.rating').barrating({readonly: true});
+            if (this.preView) {
+                this.preView.trigger('updatePreview', model);
+            }
+            this.collection.add(modelJSON, {merge: true});
+        } else {
+            $curEl.find('.noData').remove();
+            this.collection.add(modelJSON, {remove: false});
+            this.trigger('pagination',{
+                length     : ++this.collection.totalRecords,
+                currentPage: this.collection.currentPage,
+                itemsNumber: ++this.collection.pageSize
+            });
+            $thumbnailsContainer.prepend(this.templateNew({
+                model: modelJSON
             }));
-
-            this.trigger('selectedElementsChanged', {
-                length  : 0,
-                checkAll: false
-            });
-
-            App.$preLoader.fadeFn({
-                visibleState: false
-            });
         }
 
-    });
+        this.trigger('hideActionDd');
+    },
 
-    return View;
+    checkThumbnail: function (e) {
+        var $targetEl = $(e.target);
+        var $targetDivContainer = $targetEl.closest(".thumbnail");
+        var $checkbox = $targetDivContainer.find('input[type="checkbox"]');
+        var checked = $checkbox.prop('checked');
+
+        $checkbox.prop('checked', !checked);
+
+        this.inputClick(e);
+    },
+
+    showFilteredPage: function (filter) {
+        var itemsNumber = $("#itemsNumber").text();
+
+        this.filter = filter;
+
+        this.startTime = new Date();
+        this.newCollection = false;
+
+        $("#top-bar-deleteBtn").hide();
+        $('#checkAll').prop('checked', false);
+
+        this.changeLocationHash(1, itemsNumber, filter);
+        this.collection.firstPage({count: itemsNumber, filter: filter});
+    },
+
+    showMoreContent: function (newModels) {
+        var $currentEl = this.$el;
+        var $holder = $currentEl.find('.thumbnailsItems');
+
+        this.pageAnimation(this.collection.direction, $holder);
+
+        $holder.empty();
+        $holder.html(this.template({
+            collection: newModels.toJSON()
+        }));
+
+        this.trigger('selectedElementsChanged', {
+            length  : 0,
+            checkAll: false
+        });
+
+        App.$preLoader.fadeFn({
+            visibleState: false
+        });
+    }
+
 });
