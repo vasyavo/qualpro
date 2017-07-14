ebbc6e1369af53c3cfd25c0f9cf41ad661d989e7
yaroslav.fedyshyn <fedyshyn.y@gmail.com>
2017-07-14 11:30:43 +0300

    refactor(views/region): migrate files to webpack pattern.

--- src/public/js/views/region/topBarView.js
+++ src/public/js/views/region/topBarView.js
@@ -1,14 +1,10 @@
-define([
-        'text!templates/domain/topBarTemplate.html',
-        'text!templates/pagination/pagination.html',
-        'views/baseTopBar'
-    ],
-    function (topBarTemplate, pagination, baseTopBar) {
-        var TopBarView = baseTopBar.extend({
-            contentType       : "region",
-            template          : _.template(topBarTemplate),
-            paginationTemplate: _.template(pagination)
-        });
+var _ = require('underscore');
+var topBarTemplate = require('../../../templates/domain/topBarTemplate.html');
+var pagination = require('../../../templates/pagination/pagination.html');
+var baseTopBar = require('../../views/baseTopBar');
 
-        return TopBarView;
-    });
+module.exports = baseTopBar.extend({
+    contentType       : "region",
+    template          : _.template(topBarTemplate),
+    paginationTemplate: _.template(pagination)
+});
