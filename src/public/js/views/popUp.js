4c2544e83bbeddeba2d955012ba778af138ff4ae
yaroslav.fedyshyn <fedyshyn.y@gmail.com>
2017-07-14 11:57:01 +0300

    refactor(views/popUp.js): migrate files to webpack pattern.

--- src/public/js/views/popUp.js
+++ src/public/js/views/popUp.js
@@ -1,77 +1,68 @@
-'use strict';
+var _ = require('underscore');
+var Backbone = require('backbone');
+var MESSAGES = require('../constants/popUpMessages');
+var template = require('../../templates/main/popUp.html');
 
-define([
-    'backbone',
-    'jQuery',
-    'Underscore',
-    'constants/popUpMessages',
-    'text!templates/main/popUp.html'
-], function (Backbone, $, _, MESSAGES, template) {
+module.exports = Backbone.View.extend({
+    contentType: 'PopUp',
+    imageSrc   : '',
+    template   : _.template(template),
 
-    var PopUpView = Backbone.View.extend({
-        contentType: 'PopUp',
-        imageSrc   : '',
-        template   : _.template(template),
+    events: {},
 
-        events: {},
+    initialize: function (options) {
+        this.render(options);
+    },
 
-        initialize: function (options) {
-            this.render(options);
-        },
+    render: function (options) {
+        options = options || {};
 
-        render: function (options) {
-            options = options || {};
+        var contentType = options.contentType;
+        var action = options.action;
+        var title = '';
+        var message = '';
+        var saveCb = options.saveCb;
+        var saveTitle = options.saveTitle || 'OK';
+        var showCancelBtn = options.showCancelBtn || true;
+        var iconClass = options.iconClass || '';
+        var buttons = {};
+        var dialogOptions;
+        var CONTENT;
+        var ACTION;
+        var currentLanguage = App.currentUser.currentLanguage;
 
-            var contentType = options.contentType;
-            var action = options.action;
-            var title = '';
-            var message = '';
-            var saveCb = options.saveCb;
-            var saveTitle = options.saveTitle || 'OK';
-            var showCancelBtn = options.showCancelBtn || true;
-            var iconClass = options.iconClass || '';
-            var buttons = {};
-            var dialogOptions;
-            var CONTENT;
-            var ACTION;
-            var currentLanguage = App.currentUser.currentLanguage;
+        if (contentType) {
+            CONTENT = MESSAGES[contentType];
 
-            if (contentType) {
-                CONTENT = MESSAGES[contentType];
+            if (CONTENT) {
+                ACTION = CONTENT[action];
 
-                if (CONTENT) {
-                    ACTION = CONTENT[action];
-
-                    if (ACTION) {
-                        title = ACTION.title[currentLanguage];
-                        message = ACTION.message[currentLanguage];
-                    }
+                if (ACTION) {
+                    title = ACTION.title[currentLanguage];
+                    message = ACTION.message[currentLanguage];
                 }
             }
-
-            if (saveCb) {
-                buttons.save = {
-                    text : saveTitle,
-                    click: saveCb
-                };
-            }
-
-            dialogOptions = {
-                dialogClass  : 'popUpDialog',
-                title        : title,
-                showCancelBtn: showCancelBtn,
-                height       : '80%',
-                buttons      : buttons
-            };
-
-            this.$el.html(this.template({message: message, title: title, iconClass: iconClass}));
-            this.$el = this.$el.dialog(dialogOptions);
-            this.delegateEvents(this.events);
-
-            return this;
         }
-    });
 
-    return PopUpView;
+        if (saveCb) {
+            buttons.save = {
+                text : saveTitle,
+                click: saveCb
+            };
+        }
+
+        dialogOptions = {
+            dialogClass  : 'popUpDialog',
+            title        : title,
+            showCancelBtn: showCancelBtn,
+            height       : '80%',
+            buttons      : buttons
+        };
+
+        this.$el.html(this.template({message: message, title: title, iconClass: iconClass}));
+        this.$el = this.$el.dialog(dialogOptions);
+        this.delegateEvents(this.events);
+
+        return this;
+    }
 });
-
