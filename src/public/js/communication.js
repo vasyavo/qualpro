ce07a97e95e2a8f92742b6d1db5dc9f7e01e152b
yaroslav.fedyshyn <fedyshyn.y@gmail.com>
2017-07-14 12:03:17 +0300

    refactor(js/comunication.js): migrate files to webpack pattern.

--- src/public/js/communication.js
+++ src/public/js/communication.js
@@ -1,21 +1,19 @@
-define(function(require) {
-    var $ = require('jQuery');
+var $ = require('jQuery');
 
-    var checkLogin = function (callback) {
-        var url = "/authenticated";
-        $.ajax({
-            url: url,
-            type: "GET",
-            success: function () {
-                return callback(true);
-            },
-            error: function (data) {
-                return callback(false);
-            }
-        });
-    };
+var checkLogin = function (callback) {
+    var url = "/authenticated";
+    $.ajax({
+        url: url,
+        type: "GET",
+        success: function () {
+            return callback(true);
+        },
+        error: function (data) {
+            return callback(false);
+        }
+    });
+};
 
-    return {
-        checkLogin: checkLogin
-    };
-});
+module.exports = {
+    checkLogin: checkLogin
+};
