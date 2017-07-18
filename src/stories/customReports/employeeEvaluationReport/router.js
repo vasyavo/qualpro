007d809a4916860d2b2a4ccf2dd8f5f8cccd59da
Edgard Tanchenec <edgar.tanchenec@thinkmobiles.com>
2017-07-18 18:03:29 +0300

    feat(backend/employeeEvaluationCustomReport): retrieve data for main table view

--- src/stories/customReports/employeeEvaluationReport/router.js
+++ src/stories/customReports/employeeEvaluationReport/router.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
+const getAll = require('./routes/getAll');
 const getFilters = require('./routes/getFilters');
 
 const router = express.Router();
 
+router.post('/', getAll);
 router.post('/filters', getFilters);
 
 module.exports = router;
