b9e8e6958d1ff7cbb638a38b7aad6eef3d5a2136
Edgard Tanchenec <edgar.tanchenec@thinkmobiles.com>
2017-07-19 14:35:30 +0300

    feat(backend/employeeEvaluationCustomReport): retrieve personnel rating list

--- src/stories/customReports/employeeEvaluationReport/router.js
+++ src/stories/customReports/employeeEvaluationReport/router.js
@@ -1,11 +1,13 @@
 const express = require('express');
 
 const getAll = require('./routes/getAll');
+const getPersonnelRating = require('./routes/getPersonnelRating');
 const getFilters = require('./routes/getFilters');
 
 const router = express.Router();
 
 router.post('/', getAll);
+router.get('/personnelRating/:personnelId', getPersonnelRating);
 router.post('/filters', getFilters);
 
 module.exports = router;
