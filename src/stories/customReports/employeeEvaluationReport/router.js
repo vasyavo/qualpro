dc9b6b85228dafc31f4212c2dca7b933cdf4a6bd
Edgard Tanchenec <edgar.tanchenec@thinkmobiles.com>
2017-07-19 17:02:24 +0300

    feat(backend/employeeEvaluationCustomReport): retrieve chart data

--- src/stories/customReports/employeeEvaluationReport/router.js
+++ src/stories/customReports/employeeEvaluationReport/router.js
@@ -4,10 +4,12 @@ const getAll = require('./routes/getAll');
 const getPersonnelRating = require('./routes/getPersonnelRating');
 const getPersonnelActivity = require('./routes/getPersonnelActivity');
 const getFilters = require('./routes/getFilters');
+const getCharts = require('./routes/getCharts');
 
 const router = express.Router();
 
 router.post('/', getAll);
+router.post('/charts', getCharts);
 router.get('/personnelRating/:personnelId', getPersonnelRating);
 router.get('/personnelActivity/:personnelId', getPersonnelActivity);
 router.post('/filters', getFilters);
