c63eb522a0c099c5879da330c3049b0314a35962
petroShved <petro.shved@foxtrapp.com>
2017-07-27 13:46:33 +0300

    fix(newProductLaunch): fixed issue with toFixed string

--- src/stories/customReports/newProductLaunchReport/routes/XMLExport.js
+++ src/stories/customReports/newProductLaunchReport/routes/XMLExport.js
@@ -560,6 +560,7 @@ module.exports = (req, res, next) => {
                         const currentCountry = currency.defaultData.find((country) => {
                             return country._id.toString() === item.country._id.toString();
                         });
+                        item.price = Number(item.price);
                         let price = item.price.toFixed(2);
                         if (queryFilter[CONTENT_TYPES.COUNTRY].length > 1) {
                             price = parseFloat(price * currentCountry.currencyInUsd).toFixed(2);
