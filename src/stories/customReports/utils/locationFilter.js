c7447abb5231f6368f2fbd457dfca07ee122e6a8
pasha <pasha.demko@gmail.com>
2017-08-14 14:31:08 +0300

    fix(objectiveReport): added roles scope to filter
    QP-1148

--- src/stories/customReports/utils/locationFilter.js
+++ src/stories/customReports/utils/locationFilter.js
@@ -2,13 +2,31 @@ const CONTENT_TYPES = require('./../../../public/js/constants/contentType');
 
 const locations = [CONTENT_TYPES.COUNTRY, CONTENT_TYPES.REGION, CONTENT_TYPES.SUBREGION, CONTENT_TYPES.BRANCH];
 
-module.exports = (pipeline, personnel, filter) => {
+
+/**
+ * @function
+ * @param {Array} pipeline - current pipeline.
+ * @param {Object} personnel - current user .
+ * @param {Object} filter - filter from user.
+ * @param {Boolean} forFilter - condition is this function used for filter query or get docs query
+ */
+
+module.exports = (pipeline, personnel, filter, forFilter) => {
     const $locationMatch = {
         $and: [],
     };
 
     locations.forEach((location) => {
-        const locationItems = personnel[location] && personnel[location].length ? personnel[location] : filter[location];
+
+        let locationItems;
+
+        if (forFilter) {
+            locationItems = personnel[location] && personnel[location].length ? personnel[location] : null;
+            filter[location] = locationItems;
+        } else {
+            locationItems = personnel[location] && personnel[location].length ? personnel[location] : filter[location];
+        }
+
 
         if (locationItems && locationItems.length) {
             $locationMatch.$and.push({
