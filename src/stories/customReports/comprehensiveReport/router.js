41ba48fe1213090fcae2b6808fdaca3435cc1911
petroShved <petro.shved@foxtrapp.com>
2017-09-08 17:27:01 +0300

    feat(comprehensive): add variant filter

--- src/stories/customReports/comprehensiveReport/router.js
+++ src/stories/customReports/comprehensiveReport/router.js
@@ -3,11 +3,13 @@ const express = require('express');
 const locationFilters = require('./routes/locationFilters');
 const personnelFilters = require('./routes/personnelFilters');
 const productFilters = require('./routes/productFilters');
+const variantFilters = require('./routes/variantFilters');
 
 const router = express.Router();
 
 router.post('/location/filters', locationFilters);
 router.post('/employee/filters', personnelFilters);
 router.post('/product/filters', productFilters);
+router.post('/variant/filters', variantFilters);
 
 module.exports = router;
