275e6c224befea14b1174612b7ebc1875eb3770d
vasyok <vasiliy.voedilo@thinkmobiles.com>
2017-09-12 17:38:35 +0300

    brandingAndMonthlyDisplay ar

--- src/public/js/translations/ar/brandingAndMonthlyDisplay.js
+++ src/public/js/translations/ar/brandingAndMonthlyDisplay.js
@@ -5,41 +5,41 @@ define([
 ], function (_, paginationTranslation, filtersTranslation) {
     var translation = {
         // list view
-        brand      : '', // todo
-        category   : '', // todo
-        displayType: '', // todo
-        startDate  : '', // todo
-        endDate    : '', // todo
-        location   : '', // todo
+        brand      : 'arab', // todo
+        category   : 'arab', // todo
+        displayType: 'arab', // todo
+        startDate  : 'arab', // todo
+        endDate    : 'arab', // todo
+        location   : 'arab', // todo
 
         // preview
-        titlePreview : '', // todo
-        country      : '', // todo
-        region       : '', // todo
-        subRegion    : '', // todo
-        retailSegment: '', // todo
-        outlet       : '', // todo
-        branch       : '', // todo
-        description  : '', // todo
-        attachments  : '', // todo
-        files        : '', // todo
-        attachBtn    : '', // todo
-        sendBtn      : '', // todo
-        noTranslation: '', // todo
-        skipped      : '', // todo
-        commentText  : '', // todo
-        missedData   : '', // todo
+        titlePreview : 'arab', // todo
+        country      : 'arab', // todo
+        region       : 'arab', // todo
+        subRegion    : 'arab', // todo
+        retailSegment: 'arab', // todo
+        outlet       : 'arab', // todo
+        branch       : 'arab', // todo
+        description  : 'arab', // todo
+        attachments  : 'arab', // todo
+        files        : 'arab', // todo
+        attachBtn    : 'arab', // todo
+        sendBtn      : 'arab', // todo
+        noTranslation: 'arab', // todo
+        skipped      : 'arab', // todo
+        commentText  : 'arab', // todo
+        missedData   : 'arab', // todo
         edit         : 'تعديل بيانات',
-        delete: '', // todo
+        delete: 'arab', // todo
 
         // edit
         saveBtn      : 'حفظ',
-        brandingAndMonthlyDisplayEditTitle: '', // todo
+        brandingAndMonthlyDisplayEditTitle: 'arab', // todo
 
         // topBar
-        all        : '', // todo
-        okBtn      : '', // todo
-        dialogTitle: '' // todo
+        all        : 'arab', // todo
+        okBtn      : 'arab', // todo
+        dialogTitle: 'arab' // todo
     };
     return _.extend({}, paginationTranslation, filtersTranslation, translation);
 });
