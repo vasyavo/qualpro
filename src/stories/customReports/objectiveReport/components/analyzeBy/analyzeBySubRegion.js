3975d3efe3416bb9e9f834382a25acf4a233488d
oleksijfomin <oleksij.fomin@gmail.com>
2017-09-15 17:04:05 +0300

    fix(custom-reports/objectives/charts): The numbers in the bars do not match the details in the table.
    QP-1567

--- src/stories/customReports/objectiveReport/components/analyzeBy/analyzeBySubRegion.js
+++ src/stories/customReports/objectiveReport/components/analyzeBy/analyzeBySubRegion.js
@@ -1,8 +1,11 @@
 module.exports = (pipeline) => {
     pipeline.push({
-        $unwind: '$subRegion',
+        $unwind: {
+            path: '$subRegion',
+            preserveNullAndEmptyArrays: true,
+        },
     });
-    
+
     pipeline.push({
         $group: {
             _id: {
@@ -14,7 +17,144 @@ module.exports = (pipeline) => {
             count: { $sum: 1 },
         },
     });
-    
+
+    pipeline.push({
+        $group: {
+            _id: null,
+            groups: {
+                $push: {
+                    _id: {
+                        country: '$_id.country',
+                        region: '$_id.region',
+                        subRegion: { $ifNull: ['$_id.subRegion', null] }, // required map location to null
+                        status: '$_id.status',
+                    },
+                    count: '$count',
+                },
+            },
+        },
+    });
+
+    pipeline.push({
+        $project: {
+            _id: false,
+            groups: 1,
+            add: {
+                $let: {
+                    vars: {
+                        setStatus: { $setUnion: ['$groups._id.status', []] }, // pick available status
+                        groupsNull: {
+                            $filter: {
+                                input: '$groups',
+                                as: 'group',
+                                cond: {
+                                    $eq: ['$$group._id.subRegion', null],
+                                },
+                            },
+                        },
+                    },
+                    in: {
+                        // evaluate count per status
+                        $map: {
+                            input: '$$setStatus',
+                            as: 'status',
+                            in: {
+                                status: '$$status',
+                                count: {
+                                    $let: {
+                                        vars: {
+                                            // pick groups with same status
+                                            groupsSameStatus: {
+                                                $filter: {
+                                                    input: '$$groupsNull',
+                                                    as: 'group',
+                                                    cond: {
+                                                        $eq: ['$$group._id.status', '$$status'],
+                                                    },
+                                                },
+                                            },
+                                        },
+                                        in: {
+                                            $sum: '$$groupsSameStatus.count',
+                                        },
+                                    },
+                                },
+                            },
+                        },
+                    },
+                },
+            },
+        },
+    });
+
+    pipeline.push({
+        $project: {
+            groups: {
+                $let: {
+                    vars: {
+                        groupsNotNull: {
+                            $filter: {
+                                input: '$groups',
+                                as: 'group',
+                                cond: {
+                                    $ne: ['$$group._id.subRegion', null],
+                                },
+                            },
+                        },
+                    },
+                    in: {
+                        // add count to groups with same status
+                        $map: {
+                            input: '$$groupsNotNull',
+                            as: 'group',
+                            in: {
+                                _id: {
+                                    country: '$$group._id.country',
+                                    region: '$$group._id.region',
+                                    subRegion: '$$group._id.subRegion',
+                                    status: '$$group._id.status',
+                                },
+                                // evaluate new count depends on group's count and matched group with same status
+                                count: {
+                                    $let: {
+                                        vars: {
+                                            matchedGroup: {
+                                                $arrayElemAt: [{
+                                                    $filter: {
+                                                        input: '$add',
+                                                        as: 'addGroup',
+                                                        cond: {
+                                                            $eq: ['$$group._id.status', '$$addGroup.status'],
+                                                        },
+                                                    },
+                                                }, 0],
+                                            },
+                                        },
+                                        in: {
+                                            $sum: ['$$group.count', '$$matchedGroup.count'],
+                                        },
+                                    },
+                                },
+                            },
+                        },
+                    },
+                },
+            },
+        },
+    });
+
+    pipeline.push({
+        $unwind: {
+            path: '$groups',
+        },
+    });
+
+    pipeline.push({
+        $replaceRoot: {
+            newRoot: '$groups',
+        },
+    });
+
     pipeline.push({
         $lookup: {
             from: 'domains',
@@ -23,7 +163,7 @@ module.exports = (pipeline) => {
             as: 'subRegion',
         },
     });
-    
+
     pipeline.push({
         $lookup: {
             from: 'domains',
@@ -32,7 +172,7 @@ module.exports = (pipeline) => {
             as: 'region',
         },
     });
-    
+
     pipeline.push({
         $lookup: {
             from: 'domains',
@@ -41,13 +181,7 @@ module.exports = (pipeline) => {
             as: 'country',
         },
     });
-    
-    pipeline.push({
-        $match: {
-            subRegion: { $ne: [] },
-        },
-    });
-    
+
     pipeline.push({
         $project: {
             count: 1,
@@ -95,7 +229,7 @@ module.exports = (pipeline) => {
             },
         },
     });
-    
+
     pipeline.push({
         $addFields: {
             location: {
@@ -111,7 +245,7 @@ module.exports = (pipeline) => {
             },
         },
     });
-    
+
     pipeline.push({
         $group: {
             _id: '$subRegion._id',
@@ -131,7 +265,7 @@ module.exports = (pipeline) => {
             total: { $sum: '$count' },
         },
     });
-    
+
     pipeline.push({
         $addFields: {
             data: {
@@ -145,18 +279,18 @@ module.exports = (pipeline) => {
             },
         },
     });
-    
+
     pipeline.push({
         $unwind: '$data',
     });
-    
+
     pipeline.push({
         $sort: {
             'data.location.name.en': 1,
             'data.count': 1,
         },
     });
-    
+
     pipeline.push({
         $group: {
             _id: {
@@ -165,7 +299,7 @@ module.exports = (pipeline) => {
             data: { $push: '$data' },
         },
     });
-    
+
     pipeline.push({
         $project: {
             _id: 0,
@@ -173,18 +307,18 @@ module.exports = (pipeline) => {
             data: 1,
         },
     });
-    
+
     pipeline.push({
         $unwind: '$labels',
     });
-    
-    
+
+
     pipeline.push({
         $sort: {
             'labels.name.en': 1,
         },
     });
-    
+
     pipeline.push({
         $group: {
             _id: {
@@ -193,7 +327,7 @@ module.exports = (pipeline) => {
             },
         },
     });
-    
+
     pipeline.push({
         $project: {
             _id: 0,
@@ -201,7 +335,7 @@ module.exports = (pipeline) => {
             labels: '$_id.labels',
         },
     });
-    
+
     pipeline.push({
         $group: {
             _id: null,
@@ -209,7 +343,7 @@ module.exports = (pipeline) => {
             labels: { $push: '$labels' },
         },
     });
-    
+
     pipeline.push({
         $project: {
             datasets: [{ data: '$data' }],
